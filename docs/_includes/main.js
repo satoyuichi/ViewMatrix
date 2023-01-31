9fd334b0c3e8645e3f81ace5186d040b4ff91949
Yuichi Sato <satoyuichi80@gmail.com>
2023-01-31 23:55:53 +0900

    Add GUI

--- docs/_includes/main.js
+++ docs/_includes/main.js
@@ -1,10 +1,13 @@
 window.onload = (event) => {
+  const gui = new GUI();
+  gui.add( document, 'title' );
+  
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
   const canvas = document.getElementById("canvas");
   const renderer = new THREE.WebGLRenderer();
-  renderer.setSize( canvas.clientWidth, canvas.clientHeight );
+  renderer.setSize( window.innerWidth, window.innerHeight );
   canvas.appendChild( renderer.domElement );
 
   const geometry = new THREE.BoxGeometry( 1, 1, 1 );
